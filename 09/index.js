7474f7fe596806ce26110d9f40b9f0142830a671
Fábio Cavalcanti <fabiocavalcanti.mkt@gmail.com>
2023-01-26 20:47:18 -0300

    Exercício 09 Gerador de Nickname Finalizado

--- 09/index.js
+++ 09/index.js
@@ -1,1 +1,10 @@
 const nome = 'Guido Cerqueira';
+
+function nickname(nome) {
+    nome = nome.toLowerCase();
+    nome = nome.replace(" ", "");
+    nome = nome.slice(0, 12);
+    console.log(`@${nome}`);
+}
+
+nickname(nome);
