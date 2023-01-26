e57b993408e5fd22b44877a958997c9f8f8c434b
Fábio Cavalcanti <fabiocavalcanti.mkt@gmail.com>
2023-01-26 18:51:26 -0300

    Exercício 04 Sistema de formatação de dados do formulário Finalizado

--- 04/index.js
+++ 04/index.js
@@ -1,3 +1,16 @@
 let identificador = "123";
 let nome = "José silva costa";
 let email = "      jose@email.com  ";
+const nomePartes = nome.split(" ");
+nome = "";
+
+for (const letra of nomePartes) {
+    nome += letra.slice(0, 1).toUpperCase() + letra.slice(1) + " ";
+}
+if (identificador.length < 6) {
+    identificador = identificador.padStart(6, "0");
+}
+
+console.log(identificador);
+console.log(nome.trim());
+console.log(email.trim());
