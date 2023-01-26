fe9dc82cf6f9f2e876443f136570ddebda29dbfe
Fábio Cavalcanti <fabiocavalcanti.mkt@gmail.com>
2023-01-26 15:01:57 -0300

    Exercício 01 Sistema de filtro de comentários Finalizado

--- 01/index.js
+++ 01/index.js
@@ -1,1 +1,15 @@
-const comentario = "Esse COVID é muito perigoso!";
+const comentario = "Esse é COVID muito perigoso!";
+const comentario1 = "Essa pandemia é mentira";
+
+function filtro(comentario) {
+    let covid = ["covid", "Covid", "COVID"];
+    if (comentario.indexOf("COVID") != -1 || comentario.indexOf("covid") != -1 || comentario.indexOf("Covid") != -1) {
+        console.log("Comentário bloqueado por conter palavras proibidas");
+    } else if (comentario.indexOf("PANDEMIA") != -1 || comentario.indexOf("pandemia") != -1 || comentario.indexOf("Pandemia") != -1) {
+        console.log("Comentário bloqueado por conter palavras proibidas");
+    } else {
+        console.log("Comentário autorizado");
+    }
+}
+
+filtro(comentario1);
