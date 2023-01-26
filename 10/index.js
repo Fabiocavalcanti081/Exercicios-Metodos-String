1a8b3327a68b8e909ac7171c4accddefca6aaf47
Fábio Cavalcanti <fabiocavalcanti.mkt@gmail.com>
2023-01-26 20:55:35 -0300

    Exercício 10 Validação de arquivos Finalizado

--- 10/index.js
+++ 10/index.js
@@ -1,1 +1,12 @@
-const nomeArquivo = 'Foto da Familia.pdf';
+const nomeArquivo = 'Foto da Familia.png';
+
+function validaArquivo(arquivo) {
+    if (arquivo.includes("jpg") || arquivo.includes("jpeg") ||
+        arquivo.includes("gif") || arquivo.includes("png")) {
+        console.log("Arquivo válido");
+    } else {
+        console.log("Arquivo inválido");
+    }
+}
+
+validaArquivo(nomeArquivo);
