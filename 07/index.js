ec5be6f9ea7ec100be11e78f69655381941f7d1b
Fábio Cavalcanti <fabiocavalcanti.mkt@gmail.com>
2023-01-26 20:23:24 -0300

    Exercício 07 Validação de e-mail Finalizado

--- 07/index.js
+++ 07/index.js
@@ -1,1 +1,18 @@
-const email = "aluno@cubos.academy";
+const email = "jose@cubos.academy.br";
+const i = email.length - 1;
+let validaEmail = 0;
+
+for (const letra of email) {
+    if (letra === "@" || letra === ".") {
+        validaEmail++;
+    }
+}
+if (email.indexOf(".") === 0 || email.indexOf("@") === 0) {
+    console.log(`E-mail inválido.`);
+} else if (email.lastIndexOf(".") === i || email.lastIndexOf("@") === i) {
+    console.log(`E-mail inválido.`);
+} else if (validaEmail < 2) {
+    console.log(`E-mail inválido.`);
+} else {
+    console.log(`E-mail válido.`);
+}
