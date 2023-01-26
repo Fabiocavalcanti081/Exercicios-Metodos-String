7c3d67278f928ee712f952fd50a90f72ddfbd132
Fábio Cavalcanti <fabiocavalcanti.mkt@gmail.com>
2023-01-26 18:59:44 -0300

    Exercício 05 Esconder número do cartão de crédito Finalizado

--- 05/index.js
+++ 05/index.js
@@ -1,1 +1,6 @@
-const numeroCartao = '1111222233334444';
+const numeroCartao = '1111222233334444'; // 16 numeros
+const cartaoParte1 = numeroCartao.slice(0, 4);
+const cartaoParte2 = numeroCartao.slice(12, 16);
+const cartaoEscondido = cartaoParte1 + cartaoParte2.padStart(12, "*");
+
+console.log(cartaoEscondido);
