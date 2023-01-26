da423ba424482831eb222a6b7f00e22fe554da7f
Fábio Cavalcanti <fabiocavalcanti.mkt@gmail.com>
2023-01-26 18:33:16 -0300

    Exercício 03 URL amigável Finalizado

--- 03/index.js
+++ 03/index.js
@@ -1,1 +1,8 @@
 const texto = "Aprenda programar do zero na Cubos Academy";
+let textoMinusculo = texto.toLowerCase();
+
+while (textoMinusculo !== textoMinusculo.replace(" ", "-")) {
+    textoMinusculo = textoMinusculo.replace(" ", "-");
+}
+
+console.log(textoMinusculo);
