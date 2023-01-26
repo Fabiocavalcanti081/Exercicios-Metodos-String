fb0b20b1b2b7b8823cfd033ec290a41e1ce681ec
Fábio Cavalcanti <fabiocavalcanti.mkt@gmail.com>
2023-01-26 20:39:42 -0300

    Exercício 08 Remover pontuação Finalizado

--- 08/index.js
+++ 08/index.js
@@ -1,1 +1,14 @@
 const cpf = "011.022.033-44";
+
+function cpfSemPontuacao(cpf) {
+    let retirandoPontuacao = cpf.replace(".", "");
+
+    while (retirandoPontuacao !== retirandoPontuacao.replace(".", "")) {
+        retirandoPontuacao = retirandoPontuacao.replace(".", "");
+    }
+    retirandoPontuacao = retirandoPontuacao.replace("-", "");
+
+    return retirandoPontuacao;
+}
+
+console.log(cpfSemPontuacao(cpf));
